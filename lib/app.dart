c9c9625992144decb4e6ff1779afb66cab44e03a
louisthecat <louiskatze1@web.de>
2026-06-11 04:29:38 +0000

    Fix: services-Import fuer AppExitResponse

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'dart:io' show Platform;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
