f215f7df8f291559530a491e70d6659f040b583b
louisthecat <louiskatze1@web.de>
2026-07-20 03:48:08 +0000

    Fix: board_providers importieren fuer weekBoardProvider

--- lib/features/whiteboard/task_detail_sheet.dart
+++ lib/features/whiteboard/task_detail_sheet.dart
@@ -7,6 +7,7 @@ import '../../core/database/database.dart';
 import '../../core/providers/database_provider.dart';
 import '../../core/services/auto_backup_trigger.dart';
 import '../../core/services/produktion_erfassen_service.dart';
+import '../board/board_providers.dart';
 import 'whiteboard_provider.dart';
 
 /// Öffnet einen Bottom-Sheet-Dialog mit allen Details zum Task.
